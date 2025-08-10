977c115d36eae6202515acc25fd8b9026b95af6b
Balazs Sevecsek <mountain@tari.com>
2025-08-10 22:12:55 +0200

    initial

---
+++ run.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+if [ -z "$DEV_ENV" ]; then
+    echo "env var DEV_ENV needs to be present"
+    exit 1
+fi
+
+# if i just did DEV_ENV=$(pwd) ./run then this is needed for the rest of the
+# scripts
+export DEV_ENV="$DEV_ENV"
+
+grep=""
+dry_run="0"
+
+while [[ $# -gt 0 ]]; do
+    echo "ARG: \"$1\""
+    if [[ "$1" == "--dry" ]]; then
+        dry_run="1"
+    else
+        grep="$1"
+    fi
+    shift
+done
+
+log() {
+    if [[ $dry_run == "1" ]]; then
+        echo "[DRY_RUN]: $1"
+    else
+        echo "$1"
+    fi
+}
+
+log "RUN: env: $env -- grep: $grep"
+
+runs_dir=`find $script_dir/scripts -mindepth 1 -maxdepth 1 -executable`
+
+log "running script: preinstall"
+# if not dry run and not filtered out by grep
+if [[ $dry_run == "0" ]] &&  echo "preinstall" | grep -vq "$grep"; then
+    source "$script_dir/preinstall.sh"
+fi
+
+log "running script: hw"
+# if not dry run and not filtered out by grep
+if [[ $dry_run == "0" ]] &&  echo "hw" | grep -vq "$grep"; then
+    source "$script_dir/hw.sh"
+fi
+
+
+for s in $runs_dir; do
+    if basename $s | grep -vq "$grep"; then
+        log "grep \"$grep\" filtered out $s"
+        continue
+    fi
+
+    log "running script: $s"
+
+    if [[ $dry_run == "0" ]]; then
+        $s
+    fi
+done
