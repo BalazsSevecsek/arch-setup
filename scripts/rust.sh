69843be7cd831b617355d98731ed977ff7689480
Balazs Sevecsek <mountain@tari.com>
2025-08-11 22:24:22 +0200

    add sourcing zshrc

--- scripts/rust.sh
+++ scripts/rust.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile complete
+source ~/.zshrc
 cargo install stylua --features luajit
 cargo install clippy
