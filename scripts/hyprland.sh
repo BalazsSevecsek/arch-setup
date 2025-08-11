b6a764e8f7bacce33b31bbb55a2476bd525adbcf
Balazs Sevecsek <mountain@tari.com>
2025-08-11 20:13:24 +0200

    add some hyprland packages

--- scripts/hyprland.sh
+++ scripts/hyprland.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 sudo pacman -S --needed --noconfirm hyprland
+sudo pacman -S --needed --noconfirm wayland xorg-xwayland wl-clipboard mesa xdg-desktop-portal xdg-desktop-portal-wlr foot greetd
+sudo pacman -S --needed --noconfirm xdg-desktop-portal-hyprland hyprpaper waybar
+
