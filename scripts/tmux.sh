23faae81baaefb10ab3040f5c846899752336f7b
Balazs Sevecsek <mountain@tari.com>
2025-08-11 19:58:49 +0200

    add tmux

---
+++ scripts/tmux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo pacman -S --noconfirm --needed tmux
+
+if [ ! -f ~/.tmux.conf ]; then
+  cp config/tmux.conf ~/.tmux.conf
+fi
+
+#tmux plugin manager
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
