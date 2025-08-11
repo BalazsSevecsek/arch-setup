b6d516ae4d7f57e84ecf01036c1a1c64d01d1709
Balazs Sevecsek <mountain@tari.com>
2025-08-11 18:32:21 +0200

    fix yay install

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 #package manager yay
-sudo pacman -S --needed --noconfirm git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd ~
+sudo pacman -S --needed --noconfirm git base-devel 
+#if the folder of yay does not exist, clone it
+if [ ! -d "yay" ]; then
+    git clone https://aur.archlinux.org/yay.git
+    cd yay 
+    makepkg -si && cd ~
+    rmdir yay
+fi
 
 
 
