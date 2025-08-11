19efc78571e4e7eef204cd885629f9f4ecd5b872
Balazs Sevecsek <mountain@tari.com>
2025-08-11 22:28:45 +0200

    add rust check

--- scripts/rust.sh
+++ scripts/rust.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile complete
-source "$HOME/.cargo/env"
-cargo install stylua --features luajit
 
+# Check if Rust is already installed
+if [[ ! -x "$(command -v rustup)" ]]; then
+    echo "Rust is not installed, installing now..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile complete
+    source "$HOME/.cargo/env"
+    cargo install stylua --features luajit
+else
+    echo "Rust is already installed, skipping installation."
+    exit 0
+fi
