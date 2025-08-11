240c30e3c1ae7462d9978503bfa44ea6d8e1d66e
Balazs Sevecsek <mountain@tari.com>
2025-08-11 22:47:52 +0200

    add rust seg

--- scripts/rust.sh
+++ scripts/rust.sh
@@ -5,8 +5,11 @@
 if ! rustup --version; then
     echo "Rust is not installed, installing now..."
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile complete
-    source "$HOME/.cargo/env"
     cargo install stylua --features luajit
+    sed -i.bak '/^export PATH=/ {
+      /$HOME\/\.cargo\/bin/ ! s/"$/:\$HOME\/.cargo\/bin"/
+    }' ~/.zshrc
+    source "$HOME/.cargo/env"
 else
     echo "Rust is already installed, skipping installation."
     exit 0
