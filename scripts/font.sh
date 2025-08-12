06fbf88a5d1c8156cc76306a506e60827c98b3e0
Balazs Sevecsek <mountain@tari.com>
2025-08-12 16:33:57 +0200

    add extra files

---
+++ scripts/font.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo pacman -S --noconfirm --needed otf-font-awesome ttf-dejavu
--- scripts/hyprland.sh
+++ scripts/hyprland.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo pacman -S --needed --noconfirm vim kitty mesa polkit-kde-agent qt5-wayland qt6-wayland xdg-desktop-portal-hyprland xdg-utils gdm
+sudo pacman -S --needed --noconfirm mesa polkit-kde-agent qt5-wayland qt6-wayland xdg-desktop-portal-hyprland xdg-utils gdm
 sudo pacman -S --needed --noconfirm hyprland waybar hyprpaper
