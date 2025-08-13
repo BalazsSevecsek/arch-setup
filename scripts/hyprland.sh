f619ccf1ce577277e181242ab1250a0a2823804a
Balazs Sevecsek <mountain@tari.com>
2025-08-13 13:37:21 +0200

    fix issues

--- scripts/hyprland.sh
+++ scripts/hyprland.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo pacman -S --needed --noconfirm mesa polkit-kde-agent qt5-wayland qt6-wayland xdg-desktop-portal-hyprland xdg-utils gdm
-sudo pacman -S --needed --noconfirm uwsm hyprland  waybar hyprpaper hyprsunset hyprlock hypridle playerctl
+sudo pacman -S --needed --noconfirm uwsm hyprland  waybar hyprpaper hyprsunset hyprlock hypridle playerctl hypershot walker mako fcitx5 swaybg swayosd-server
 # yay -S --needed --noconfirm nerd-fonts-caskaydia-mono
 
 cp  config/hyprland/* ~/.config/hypr/
