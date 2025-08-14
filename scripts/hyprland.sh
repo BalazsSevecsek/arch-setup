7db8e3939e38eb0f8bb5894aa43fab879368f7fb
Balazs Sevecsek <mountain@tari.com>
2025-08-14 10:53:03 +0200

    fix

--- scripts/hyprland.sh
+++ scripts/hyprland.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo pacman -S --needed --noconfirm mesa polkit-kde-agent qt5-wayland qt6-wayland xdg-desktop-portal-hyprland xdg-utils gdm
-sudo pacman -S --needed --noconfirm uwsm hyprland  waybar hyprpaper hyprsunset hyprlock hypridle playerctl hypershot walker mako fcitx5 swaybg swayosd-server
+set -e 
+sudo pacman -S --needed --noconfirm mesa polkit-kde-agent qt5-wayland qt6-wayland xdg-desktop-portal-hyprland xdg-utils gdm || exit 1
+sudo pacman -S --needed --noconfirm uwsm hyprland  waybar hyprpaper hyprsunset hyprlock hypridle playerctl hypershot walker mako fcitx5 swaybg swayosd-server || exit 1
 # yay -S --needed --noconfirm nerd-fonts-caskaydia-mono
 
 cp  config/hyprland/* ~/.config/hypr/
