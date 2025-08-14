4f8d84c91dfb1c10340023bd871b2eee7b2ad825
Balazs Sevecsek <mountain@tari.com>
2025-08-14 10:56:53 +0200

    fix

--- scripts/hyprland.sh
+++ scripts/hyprland.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-set -e 
-sudo pacman -S --needed --noconfirm mesa polkit-kde-agent qt5-wayland qt6-wayland xdg-desktop-portal-hyprland xdg-utils gdm || exit 1
-sudo pacman -S --needed --noconfirm uwsm hyprland  waybar hyprpaper hyprsunset hyprlock hypridle playerctl hypershot walker mako fcitx5 swaybg swayosd-server || exit 1
+sudo pacman -S --needed --noconfirm mesa
+sudo pacman -S --needed --noconfirm polkit-kde-agent
+sudo pacman -S --needed --noconfirm qt5-wayland
+sudo pacman -S --needed --noconfirm qt6-wayland
+sudo pacman -S --needed --noconfirm xdg-desktop-portal-hyprland
+sudo pacman -S --needed --noconfirm xdg-utils
+sudo pacman -S --needed --noconfirm gdm
+sudo pacman -S --needed --noconfirm uwsm 
+sudo pacman -S --needed --noconfirm hyprland  
+sudo pacman -S --needed --noconfirm waybar
+sudo pacman -S --needed --noconfirm hyprpaper
+sudo pacman -S --needed --noconfirm hyprsunset
+sudo pacman -S --needed --noconfirm hyprlock
+sudo pacman -S --needed --noconfirm hypridle
+sudo pacman -S --needed --noconfirm playerctl
+sudo pacman -S --needed --noconfirm hypershot
+sudo pacman -S --needed --noconfirm walker
+sudo pacman -S --needed --noconfirm mako
+sudo pacman -S --needed --noconfirm fcitx5
+sudo pacman -S --needed --noconfirm swaybg
+sudo pacman -S --needed --noconfirm swayosd-server
+
 # yay -S --needed --noconfirm nerd-fonts-caskaydia-mono
 
 cp  config/hyprland/* ~/.config/hypr/
