a5f2ab71ba3e9c042d430311b5dc6d111ab35df2
Balazs Sevecsek <mountain@tari.com>
2025-08-15 22:11:05 +0200

    fix

--- scripts/hyprland.sh
+++ scripts/hyprland.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-sudo pacman -S --needed --noconfirm harfbuzz
 sudo pacman -S --needed --noconfirm dolphin
 sudo pacman -S --needed --noconfirm ghostty
-sudo pacman -S --needed --noconfirm kitty
 sudo pacman -S --needed --noconfirm foot
 sudo pacman -S --needed --noconfirm mesa
 sudo pacman -S --needed --noconfirm polkit-kde-agent
@@ -12,7 +10,6 @@ sudo pacman -S --needed --noconfirm qt6-wayland
 sudo pacman -S --needed --noconfirm xdg-desktop-portal-hyprland
 sudo pacman -S --needed --noconfirm xdg-utils
 sudo pacman -S --needed --noconfirm gdm
-sudo pacman -S --needed --noconfirm uwsm 
 sudo pacman -S --needed --noconfirm hyprland  
 sudo pacman -S --needed --noconfirm waybar
 sudo pacman -S --needed --noconfirm hyprpaper
@@ -22,9 +19,8 @@ sudo pacman -S --needed --noconfirm hypridle
 sudo pacman -S --needed --noconfirm playerctl
 sudo pacman -S --needed --noconfirm hypershot
 sudo pacman -S --needed --noconfirm walker
-sudo pacman -S --needed --noconfirm mako
-sudo pacman -S --needed --noconfirm fcitx5
-sudo pacman -S --needed --noconfirm swaybg
+sudo pacman -S --needed --noconfirm blueman-manager
+sudo pacman -S --needed --noconfirm pavucontrol
 # sudo pacman -S --needed --noconfirm swayosd-server
 
 # yay -S --needed --noconfirm nerd-fonts-caskaydia-mono
