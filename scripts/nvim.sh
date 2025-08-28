a8f861c9d9950d31240eb542ec5338adbd4bd461
Balazs Sevecsek <balazs.sevecsek@gmail.com>
2025-08-28 13:32:49 +0200

    add nvim

---
+++ scripts/nvim.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+sudo pacman -S --needed --noconfirm vim nvim
+
+#backup existing nvim config if any
+
+# required
+mv ~/.config/nvim{,.bak}
+
+# optional but recommended
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+
+if [ ! -d ~/.config/nvim ]; then
+    git clone https://github.com/LazyVim/starter ~/.config/nvim
+fi
+
+rm -rf ~/.config/nvim/.git
