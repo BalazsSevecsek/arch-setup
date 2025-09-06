e67ae3aa7aa3cc5f3cf7c3cd0382a74721a564bc
Balazs Sevecsek <balazs.sevecsek@gmail.com>
2025-09-06 12:19:52 +0200

    fix iwlwifi

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ ! -f ~/.ssh/id_ed25519 ]; then
-  ssh-keygen -t ed25519 -C "emailaddress" -N "" -f ~/.ssh/id_ed25519
-  echo "Add this to github keys"
-  cat ~/.ssh/id_ed25519.pub
+    ssh-keygen -t ed25519 -C "emailaddress" -N "" -f ~/.ssh/id_ed25519
+    echo "Add this to github keys"
+    cat ~/.ssh/id_ed25519.pub
 fi
 
 git config --global user.email "balazs.sevecsek@gmail.com"
@@ -12,22 +12,22 @@ git config --global user.name "Balazs Sevecsek"
 #package manager
 #install yay if it is not already installed
 if ! pacman -Q yay &>/dev/null; then
-  sudo pacman -S --needed  --noconfirm git base-devel
-  git clone https://aur.archlinux.org/yay-bin.git
-  cd yay-bin
-  makepkg -si --noconfirm
-  cd ..
-  rm -rf yay-bin
+    sudo pacman -S --needed  --noconfirm git base-devel
+    git clone https://aur.archlinux.org/yay-bin.git
+    cd yay-bin
+    makepkg -si --noconfirm
+    cd ..
+    rm -rf yay-bin
 else
-  echo "yay is already installed."
+    echo "yay is already installed."
 fi
 
-#GPU drivers 
+#GPU drivers
 # Detect GPU vendor
 GPU_VENDOR="none"
 if lspci | grep -i 'nvidia' &> /dev/null; then
     GPU_VENDOR="nvidia"
-elif lspci | grep -i 'amd/ati' &> /dev/null; then
+    elif lspci | grep -i 'amd/ati' &> /dev/null; then
     GPU_VENDOR="amd"
 fi
 
@@ -36,19 +36,19 @@ echo "Detected GPU vendor: $GPU_VENDOR"
 # Install drivers based on detected vendor
 sudo pacman -S --noconfirm vulkan-icd-loader vulkan-tools
 case $GPU_VENDOR in
-  nvidia)
-    echo "Installing Nvidia drivers..."
-    sudo pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
-    # Optionally install dkms for kernel module auto rebuild
-    sudo pacman -S --noconfirm dkms
+    nvidia)
+        echo "Installing Nvidia drivers..."
+        sudo pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+        # Optionally install dkms for kernel module auto rebuild
+        sudo pacman -S --noconfirm dkms
     ;;
-  amd)
-    echo "Installing AMD drivers..."
-    # AMD GPU open source drivers are part of mesa
-    sudo pacman -S --noconfirm xf86-video-amdgpu mesa vulkan-radeon
+    amd)
+        echo "Installing AMD drivers..."
+        # AMD GPU open source drivers are part of mesa
+        sudo pacman -S --noconfirm xf86-video-amdgpu mesa vulkan-radeon
     ;;
-  *)
-    echo "No Nvidia or AMD GPU detected, skipping driver installation."
+    *)
+        echo "No Nvidia or AMD GPU detected, skipping driver installation."
     ;;
 esac
 
@@ -64,10 +64,10 @@ echo "Driver installation script finished."
 sudo pacman  -S --needed --noconfirm zsh
 #if current shell is not zsh, change it to zsh
 if [ "$SHELL" != "/bin/zsh" ]; then
-  echo "Changing default shell to zsh..."
-  hash -r
-  sudo chsh -s /bin/zsh $(whoami)
-  yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    echo "Changing default shell to zsh..."
+    hash -r
+    sudo chsh -s /bin/zsh $(whoami)
+    yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 
@@ -75,3 +75,10 @@ source ./keepassxc.sh
 sudo pacman -S --needed --noconfirm flatpak
 yay -Sy -needed --noconfirm brave-bin
 sudo pacman -S --needed --noconfirm wl-clipboard
+
+#for wifi
+sudo pacman -S --needed --noconfirm linux-firmware
+
+sudo cp ./config/hyprland/iwlwifi/iwlwifi.conf /etc/modprobe.d/iwlwifi.conf
+sudo modprobe -r iwlmvm iwlwifi
+sudo modprobe iwlwifi
