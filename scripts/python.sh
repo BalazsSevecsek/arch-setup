35122bbe75792d3070af898cf94f2a871f58b688
Balazs Sevecsek <balazs.sevecsek@gmail.com>
2025-09-12 11:43:04 +0200

    add pyenv

--- scripts/python.sh
+++ scripts/python.sh
@@ -6,3 +6,5 @@ curl -fsSL https://pyenv.run | bash
 grep -qxF 'export PYENV_ROOT="$HOME/.pyenv"' ~/.zshrc || echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 grep -qxF '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' ~/.zshrc || echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 grep -qxF 'eval "$(pyenv init - zsh)"' ~/.zshrc || echo 'eval "$(pyenv init - zsh)"' >> ~/.zshrc
+
+source ~/.zshrc
